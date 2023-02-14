929849f7dfe8f643742ef27153ce76042ee39dc6
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-14 09:13:08 +0200

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id   = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return self.name
+
+class Sales(models.Model):
+    id      = models.AutoField(primary_key=True)
+    product = models.OneToOneField(Product, on_delete=models.DO_NOTHING)
+    note    = models.CharField(max_length = 100, unique = True)
+
+    def __str__(self):
+        return str(self.id) + self.product.name
