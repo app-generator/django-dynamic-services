c85c99a706468c88b47968949f2af0e821b25383
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-14 09:13:30 +0200

    Create views.py

---
+++ api/views.py
@@ -0,0 +1,156 @@
+from http import HTTPStatus
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+from api.serializers import *
+
+
+from home.models import Product
+
+from home.models import Sales
+
+
+
+class ProductView(APIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.POST)
+        if not serializer.is_valid():
+            return Response(data={
+                **serializer.errors,
+                'success': False
+            }, status=HTTPStatus.BAD_REQUEST)
+        serializer.save()
+        return Response(data={
+            'message': 'Record Created.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def get(self, request, pk=None):
+        if not pk:
+            return Response({
+                'data': [ProductSerializer(instance=obj).data for obj in Product.objects.all()],
+                'success': True
+            }, status=HTTPStatus.OK)
+        try:
+            obj = get_object_or_404(Product, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        return Response({
+            'data': ProductSerializer(instance=obj).data,
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def put(self, request, pk):
+        try:
+            obj = get_object_or_404(Product, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        serializer = ProductSerializer(instance=obj, data=request.POST, partial=True)
+        if not serializer.is_valid():
+            return Response(data={
+                **serializer.errors,
+                'success': False
+            }, status=HTTPStatus.BAD_REQUEST)
+        serializer.save()
+        return Response(data={
+            'message': 'Record Updated.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def delete(self, request, pk):
+        try:
+            obj = get_object_or_404(Product, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        obj.delete()
+        return Response(data={
+            'message': 'Record Deleted.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
+
+class SalesView(APIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def post(self, request):
+        serializer = SalesSerializer(data=request.POST)
+        if not serializer.is_valid():
+            return Response(data={
+                **serializer.errors,
+                'success': False
+            }, status=HTTPStatus.BAD_REQUEST)
+        serializer.save()
+        return Response(data={
+            'message': 'Record Created.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def get(self, request, pk=None):
+        if not pk:
+            return Response({
+                'data': [SalesSerializer(instance=obj).data for obj in Sales.objects.all()],
+                'success': True
+            }, status=HTTPStatus.OK)
+        try:
+            obj = get_object_or_404(Sales, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        return Response({
+            'data': SalesSerializer(instance=obj).data,
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def put(self, request, pk):
+        try:
+            obj = get_object_or_404(Sales, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        serializer = SalesSerializer(instance=obj, data=request.POST, partial=True)
+        if not serializer.is_valid():
+            return Response(data={
+                **serializer.errors,
+                'success': False
+            }, status=HTTPStatus.BAD_REQUEST)
+        serializer.save()
+        return Response(data={
+            'message': 'Record Updated.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
+    def delete(self, request, pk):
+        try:
+            obj = get_object_or_404(Sales, pk=pk)
+        except Http404:
+            return Response(data={
+                'message': 'object with given id not found.',
+                'success': False
+            }, status=HTTPStatus.NOT_FOUND)
+        obj.delete()
+        return Response(data={
+            'message': 'Record Deleted.',
+            'success': True
+        }, status=HTTPStatus.OK)
+
