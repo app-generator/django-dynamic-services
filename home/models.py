cd5ab2c1052ed31d006a10f98ffefbee1635b711
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-15 08:36:57 +0200

    Update models.py

--- home/models.py
+++ home/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Product(models.Model):
     id   = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 100) # unique = True, might affect DataTB 
+    name = models.CharField(max_length = 100) 
+    # info = models.CharField(max_length = 100, default = '')
 
     def __str__(self):
         return self.name
@@ -12,14 +13,7 @@ class Product(models.Model):
 class Sales(models.Model):
     id      = models.AutoField(primary_key=True)
     product = models.OneToOneField(Product, on_delete=models.DO_NOTHING)
-    note    = models.CharField(max_length = 100) # unique = True, might affect DataTB
+    note    = models.CharField(max_length = 100)
 
     def __str__(self):
         return str(self.id) + self.product.name
-
-class Product2(models.Model):
-    id   = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 100) # unique = True, might affect DataTB 
-
-    def __str__(self):
-        return self.name
