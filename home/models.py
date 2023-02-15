6fb205b3bab4fe09f99aad215076304e20062540
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-15 11:17:52 +0200

    Update models.py

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     id   = models.AutoField(primary_key=True)
     name = models.CharField(max_length = 100) 
-    info = models.CharField(max_length = 100, default = '')
+    # info = models.CharField(max_length = 100, default = '')
 
     def __str__(self):
         return self.name
