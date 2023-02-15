ccdebc77ffd96a213daa399291e3f3e27bfceb9d
App Generator <51070104+app-generator@users.noreply.github.com>
2023-02-15 08:38:11 +0200

    Update views.py

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+def api_generator(request):
+    return render(request, 'pages/api-generator.html')
+
+def dyn_datatb(request):
+    return render(request, 'pages/dyn-datatb.html')
