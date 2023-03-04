be4084bc96b00ff1feded352a89d39a5f0a82bd5
App Generator <51070104+app-generator@users.noreply.github.com>
2023-03-04 09:34:03 +0200

    Dummy

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test-datatb/', views.test_datatb)
     path('api-gen'   , views.api_generator, name='api_generator'),
     path('dyn-datatb', views.dyn_datatb   , name='dyn_datatb'   ),
 ]
