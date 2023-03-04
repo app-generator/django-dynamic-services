87d6535fa7164a0414b693a1eaeebc757dcd08af
App Generator <51070104+app-generator@users.noreply.github.com>
2023-03-04 09:33:56 +0200

    Dummy

--- home/views.py
+++ home/views.py
@@ -4,21 +4,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-ddt = DataTB(model_class_path="home.models.Product")
-
-
 def index(request):
     # Page from the theme
     return render(request, 'pages/index.html')
-
-
-def test_datatb(request, **kwargs):
-    return render(request, 'test-datatb.html', context={
-        'data_table1': ddt.render(),
-    })
 
 def api_generator(request):
     return render(request, 'pages/api-generator.html')
 
 def dyn_datatb(request):
-    return render(request, 'pages/dyn-datatb.html')
+
+    context = {} 
+        
+    ddt = DataTB(model_class_path="home.models.Product")
+    context['data_table1'] = ddt.render()
+
+    return render(request, 'pages/dyn-datatb.html', context=context)
